6e114d2050b5a6340a0c5169b9cd16bf27834081
Kurniawan Adhi Wijaya <koadhyow@gmail.com>
2025-07-06 16:16:56 +0700

    Finished Expense App

--- provider_expense_app/lib/main.dart
+++ provider_expense_app/lib/main.dart
@@ -1,24 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_expense_app/models/database_provider.dart';
+import 'package:provider_expense_app/screens/all_expenses.dart';
+import 'package:provider_expense_app/screens/category_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => DatabaseProvider(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Expense App',
       debugShowCheckedModeBanner: false,
+      initialRoute: CategoryScreen.name,
+      routes: {
+        CategoryScreen.name: (_) => const CategoryScreen(),
+        AllExpenses.name: (_) => const AllExpenses(),
+      },
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
-
