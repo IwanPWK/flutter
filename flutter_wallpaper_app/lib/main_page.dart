5bde0387cb7261dd16daf43d6c7791870f81699d
Kurniawan Adhi Wijaya <koadhyow@gmail.com>
2025-07-21 12:35:09 +0700

    Masonry GridView

--- flutter_wallpaper_app/lib/main_page.dart
+++ flutter_wallpaper_app/lib/main_page.dart
@@ -1,4 +1,9 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
+import 'modal/modal.dart';
+import 'repo/repository.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -8,10 +13,28 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  Repository repo = Repository();
+  ScrollController scrollController = ScrollController();
+  late Future<List<Images>> imagesList;
+  int pageNumber = 1;
+
+  @override
+  void initState() {
+    imagesList = repo.getImagesList(pageNumber: pageNumber);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           mainAxisSize: MainAxisSize.min,
@@ -35,10 +58,75 @@ class _MainPageState extends State<MainPage> {
           ],
         ),
       ),
-      body: Center(
-        child: Text(
-          'Welcome to the Main Page!',
-          style: Theme.of(context).textTheme.headlineMedium,
+      body: SingleChildScrollView(
+        controller: scrollController,
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: [
+            FutureBuilder(
+              future: imagesList,
+              builder: ((context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return Center(
+                    child: Text('Something went wrong: ${snapshot.error}'),
+                  );
+                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                  return Center(child: Text('No images found.'));
+                }
+
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 5),
+                      child: MasonryGridView.count(
+                        controller: scrollController,
+                        itemCount: snapshot.data?.length,
+                        shrinkWrap: true,
+                        mainAxisSpacing: 5,
+                        crossAxisSpacing: 5,
+                        crossAxisCount: 2,
+                        itemBuilder: (context, index) {
+                          double height = (index % 10 + 1) * 100;
+                          return GestureDetector(
+                            onTap: () {},
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: CachedNetworkImage(
+                                fit: BoxFit.cover,
+                                height: height > 300 ? 300 : height,
+                                imageUrl:
+                                    snapshot.data![index].imagePotraitPath,
+                                errorWidget:
+                                    (context, url, error) =>
+                                        const Icon(Icons.error),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    MaterialButton(
+                      minWidth: double.infinity,
+                      padding: const EdgeInsets.symmetric(vertical: 20),
+                      color: Colors.blue,
+                      textColor: Colors.white,
+                      onPressed: () {
+                        pageNumber++;
+                        imagesList = repo.getImagesList(pageNumber: pageNumber);
+                        setState(() {});
+                      },
+                      child: const Text('Load More'),
+                    ),
+                  ],
+                );
+              }),
+            ),
+          ],
         ),
       ),
     );
