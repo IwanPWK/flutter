71cf42b5e96386df86b60c60461de79d739b0083
Kurniawan Adhi Wijaya <koadhyow@gmail.com>
2025-09-03 18:45:13 +0700

    tester

---
+++ flutter_change_tracking/android/app/src/main/kotlin/com/example/flutter_change_tracking/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_change_tracking
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ flutter_pluto_menu_bar/android/app/src/main/kotlin/com/example/flutter_pluto_menu_bar/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_pluto_menu_bar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
